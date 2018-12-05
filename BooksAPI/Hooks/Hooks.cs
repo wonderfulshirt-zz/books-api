1140e93e82efca221310bea4b0c5dc4de3438780
Andrew Slavin <andrew.slavin@sage.com>
2018-12-05 15:52:45 +0000

    Add hooks to launch and exit the API console app

---
+++ BooksAPI/Hooks/Hooks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using BooksAPI.LibraryManager;
+
+namespace BooksAPI.TestHooks
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+             
+        [BeforeTestRun]
+        public static void InitializeClient()
+        {
+            Console.WriteLine("** [BeforeTestRun]");
+            LibraryManagerUtils libraryManager = new LibraryManagerUtils();
+            libraryManager.LaunchLibraryManager();
+        }
+
+        [AfterTestRun]
+        public static void ExitClient()
+        {
+            Console.WriteLine("** [AfterTestRun]");
+            LibraryManagerUtils libraryManager = new LibraryManagerUtils();
+            libraryManager.ExitLibraryManager();
+        }
+    }
+}
---
+++ BooksAPI/LibraryManager/LibraryManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksAPI.LibraryManager
+{
+    public class LibraryManagerUtils
+    {
+        public void LaunchLibraryManager()
+        {
+            Process.Start("C:\\Users\\andrew.slavin\\OneDrive - Sage Software, Inc\\Bede\\repos\\BooksAPI\\BooksAPI\\LibraryManager\\LibraryManager.exe");
+        }
+
+        public void ExitLibraryManager()
+        {
+            //Process[] processlist = Process.GetProcesses();
+        
+            //foreach (Process theprocess in processlist)
+            //{
+            //    Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
+            //}
+
+            foreach (var process in Process.GetProcessesByName("LibraryManager"))
+            {
+                process.Kill();
+            }
+        }
+    }
+}
